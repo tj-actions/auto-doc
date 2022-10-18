77ad42d18733e3c7196c716e65222e076cf5ead0
Tonye Jack <jtonye@ymail.com>
2022-10-18 15:26:58 -0600

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,29 +12,29 @@ for output_column in "${OUTPUT_COLUMNS[@]}"; do
   EXTRA_ARGS="${EXTRA_ARGS} --outputColumns ${output_column}"
 done
 
-if [[ -z "$BIN_PATH" ]]; then
+if [[ -z "$INPUT_BIN_PATH" ]]; then
   TEMP_DIR=$(mktemp -d)
   curl -sf https://gobinaries.com/github.com/tj-actions/auto-doc | PREFIX=$TEMP_DIR sh
 
-  BIN_PATH="$TEMP_DIR/auto-doc"
+  INPUT_BIN_PATH="$TEMP_DIR/auto-doc"
 
   # Remove the temp directory on exit.
   trap 'rm -rf "$TEMP_DIR"' EXIT
 fi
 
-chmod +x "$BIN_PATH"
+chmod +x "$INPUT_BIN_PATH"
 
-$BIN_PATH --version
+$INPUT_BIN_PATH --version
 
-echo "::debug::Generating documentation using ${BIN_PATH}..."
+echo "::debug::Generating documentation using ${INPUT_BIN_PATH}..."
 echo "::debug::Extra args: ${EXTRA_ARGS}"
 
-$BIN_PATH --action="$INPUT_ACTION" --output="$INPUT_OUTPUT" \
+$INPUT_BIN_PATH --action="$INPUT_ACTION" --output="$INPUT_OUTPUT" \
   --colMaxWidth="$INPUT_COL_MAX_WIDTH" --colMaxWords="$INPUT_COL_MAX_WORDS" \
   "${EXTRA_ARGS}" && exit_status=$? || exit_status=$?
 
 # Remove the bin path if it still exists.
-[[ -f "$BIN_PATH" ]] && rm -f "$BIN_PATH"
+[[ -f "$INPUT_BIN_PATH" ]] && rm -f "$INPUT_BIN_PATH"
 
 if [[ $exit_status -ne 0 ]]; then
   echo "::warning::Error occurred running auto-doc"
