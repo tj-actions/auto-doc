a9fb2677ede3ec015d27960b3bd6c5b77917c87a
Tonye Jack <jtonye@ymail.com>
2023-05-04 05:37:07 -0600

    Update word_wrap.go

--- internal/utils/word_wrap.go
+++ internal/utils/word_wrap.go
@@ -53,7 +53,7 @@ func WordWrap(s string, limit int) string {
 			limit = len(strSlice)
 		}
 
-		result = result + strings.Join(strSlice[:limit], " ") + "<br>"
+		result = result + strings.Join(strSlice[:limit], " ") + " <br>"
 
 		// discard the elements that were copied over to result
 		strSlice = strSlice[limit:]
