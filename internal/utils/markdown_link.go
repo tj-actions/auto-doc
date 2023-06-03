dad342d3df80ddb6037b49d690b9ed5d24d99a91
Viacheslav Kudinov <viacheslav@kudinov.tech>
2023-06-03 19:04:58 +0200

    Fix "should be of the form"

--- internal/utils/markdown_link.go
+++ internal/utils/markdown_link.go
@@ -16,7 +16,7 @@ limitations under the License.
 */
 package utils
 
-// Formats string input as markdown link (named anchor) and return the link
+// MarkdownLink formats input, output and secret name to markdown link (named anchor) and return the link
 func MarkdownLink(s string, t string) string {
 	var link string
 	var typeName = t + "_" + s
