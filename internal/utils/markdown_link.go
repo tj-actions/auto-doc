c4b6c8fcba6be70eb33fd8531b2507252927341f
Viacheslav Kudinov <viacheslav@kudinov.tech>
2023-06-03 18:51:04 +0200

    Fixed "should omit type string"

--- internal/utils/markdown_link.go
+++ internal/utils/markdown_link.go
@@ -19,7 +19,7 @@ package utils
 // format string input as markdown link (named anchor)
 func MarkdownLink(s string, t string) string {
 	var link string
-	var typeName string = t + "_" + s
+	var typeName = t + "_" + s
 
 	link = "<a name=\"" + typeName + "\"></a>[" + s + "](#" + typeName + ")"
 
