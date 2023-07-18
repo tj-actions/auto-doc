6a2669fed415ebb5749c34f713ea8abdac4407af
Tonye Jack <jtonye@ymail.com>
2023-07-18 00:45:13 -0600

    feat: increase test coverage and fix bug with inputs (#498)

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,55 +7,55 @@ REUSABLE="$INPUT_REUSABLE"
 VERSION="$INPUT_VERSION"
 
 # action.yml file
-INPUT_COLUMNS=()
-if [[ -n "$INPUT_INPUT_COLUMNS" ]]; then
-  IFS=$'\n' read -rd '' -a INPUT_COLUMNS <<<"$INPUT_INPUT_COLUMNS"\
+IFS=$'\n' read -rd '' -a INPUT_COLUMNS <<<"$INPUT_INPUT_COLUMNS" || true
 
+if [[ ${#INPUT_COLUMNS[@]} -gt 0 ]]; then
   for input_column in "${INPUT_COLUMNS[@]}"; do
-    EXTRA_ARGS="${EXTRA_ARGS} --inputColumns ${input_column}"
+    EXTRA_ARGS="${EXTRA_ARGS} --inputColumns=${input_column}"
   done
 fi
 
-OUTPUT_COLUMNS=()
-if [[ -n "$INPUT_OUTPUT_COLUMNS" ]]; then
-  IFS=$'\n' read -rd '' -a OUTPUT_COLUMNS <<<"$INPUT_OUTPUT_COLUMNS"
+IFS=$'\n' read -rd '' -a OUTPUT_COLUMNS <<<"$INPUT_OUTPUT_COLUMNS" || true
 
+if [[ ${#OUTPUT_COLUMNS[@]} -gt 0 ]]; then
   for output_column in "${OUTPUT_COLUMNS[@]}"; do
-    EXTRA_ARGS="${EXTRA_ARGS} --outputColumns ${output_column}"
+    EXTRA_ARGS="${EXTRA_ARGS} --outputColumns=${output_column}"
   done
 fi
 
 # reusable workflow
-REUSABLE_SECRET_COLUMNS=()
-if [[ -n "$INPUT_REUSABLE_SECRET_COLUMNS" ]]; then
-  IFS=$'\n' read -rd '' -a REUSABLE_SECRET_COLUMNS <<<"$INPUT_REUSABLE_SECRET_COLUMNS"
+IFS=$'\n' read -rd '' -a REUSABLE_SECRET_COLUMNS <<<"$INPUT_REUSABLE_SECRET_COLUMNS" || true
 
+if [[ ${#REUSABLE_SECRET_COLUMNS[@]} -gt 0 ]]; then
   for reusable_secret_column in "${REUSABLE_SECRET_COLUMNS[@]}"; do
-    EXTRA_ARGS="${EXTRA_ARGS} --reusableSecretColumns ${reusable_secret_column}"
+    EXTRA_ARGS="${EXTRA_ARGS} --reusableSecretColumns=${reusable_secret_column}"
   done
 fi
 
-REUSABLE_INPUT_COLUMNS=()
-if [[ -n "$INPUT_REUSABLE_INPUT_COLUMNS" ]]; then
-  IFS=$'\n' read -rd '' -a REUSABLE_INPUT_COLUMNS <<<"$INPUT_REUSABLE_INPUT_COLUMNS"
+IFS=$'\n' read -rd '' -a REUSABLE_INPUT_COLUMNS <<<"$INPUT_REUSABLE_INPUT_COLUMNS" || true
 
+if [[ ${#REUSABLE_INPUT_COLUMNS[@]} -gt 0 ]]; then
   for reusable_input_column in "${REUSABLE_INPUT_COLUMNS[@]}"; do
-    EXTRA_ARGS="${EXTRA_ARGS} --reusableInputColumns ${reusable_input_column}"
+    EXTRA_ARGS="${EXTRA_ARGS} --reusableInputColumns=${reusable_input_column}"
   done
 fi
 
-REUSABLE_OUTPUT_COLUMNS=()
-if [[ -n "$INPUT_REUSABLE_OUTPUT_COLUMNS" ]]; then
-  IFS=$'\n' read -rd '' -a REUSABLE_OUTPUT_COLUMNS <<<"$INPUT_REUSABLE_OUTPUT_COLUMNS"
+IFS=$'\n' read -rd '' -a REUSABLE_OUTPUT_COLUMNS <<<"$INPUT_REUSABLE_OUTPUT_COLUMNS" || true
 
+if [[ ${#REUSABLE_OUTPUT_COLUMNS[@]} -gt 0 ]]; then
   for reusable_output_column in "${REUSABLE_OUTPUT_COLUMNS[@]}"; do
-    EXTRA_ARGS="${EXTRA_ARGS} --reusableOutputColumns ${reusable_output_column}"
+    EXTRA_ARGS="${EXTRA_ARGS} --reusableOutputColumns=${reusable_output_column}"
   done
 fi
 
 if [[ ! -f "$INPUT_FILENAME" ]]; then
   echo "::warning::No file found at: $INPUT_FILENAME"
   exit 0
+fi
+
+if [[ ! -f "$BIN_PATH" ]]; then
+  echo "::error::No binary found at: $BIN_PATH"
+  exit 1
 fi
 
 # reusable workflow
@@ -75,8 +75,6 @@ echo "::debug::Extra args: ${EXTRA_ARGS}"
 $BIN_PATH --filename="$INPUT_FILENAME" --output="$INPUT_OUTPUT" \
   --colMaxWidth="$INPUT_COL_MAX_WIDTH" --colMaxWords="$INPUT_COL_MAX_WORDS" \
   ${EXTRA_ARGS} && exit_status=$? || exit_status=$?
-
-rm -f "$BIN_PATH"
 
 if [[ $exit_status -ne 0 ]]; then
   echo "::warning::Error occurred running auto-doc"
